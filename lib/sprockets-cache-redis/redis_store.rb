fe11b56d7f9f7fcfd8d993d43d1dd2ca1ab97c7c
Carl Hörberg <carl.hoerberg@gmail.com>
2011-09-10 14:37:00 +0200

    Some documentation

--- lib/sprockets-cache-redis/redis_store.rb
+++ lib/sprockets-cache-redis/redis_store.rb
@@ -2,17 +2,23 @@ require 'digest/md5'
 
 module Sprockets
   module Cache
+    # A simple Redis cache store.
+    #
+    #     environment.cache = Sprockets::Cache::RedisStore.new($redis)
+    #
     class RedisStore
       def initialize(redis_conn, key_prefix = 'sprockets')
         @redis = redis_conn
         @key_prefix = key_prefix
       end
 
+      # Lookup value in cache
       def [](key)
         data = @redis.get path_for(key)
         Marshal.load data if data
       end
 
+      # Save value to cache
       def []=(key, value)
         @redis.set path_for(key), Marshal.dump(value)
       end
